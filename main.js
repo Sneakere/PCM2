d946d0b2902cc21f7eb64f33315abdd48ffdf517
Sebastian <ssalasg.mm@gmail.com>
2024-08-26 14:28:42 -0400

    falta todavia

---
+++ main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $("#gato-hero").hover(function () {
+        $(this).css({
+            "transform": "scale(1.3)",
+            'border-bottom': "1px solid white",
+            'transition': "all 2s ease",
+            "margin-bottom": "3rem"
+        })
+    })
+})

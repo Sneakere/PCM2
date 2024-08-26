4b600925854a621e15b6d02805404e8e32f7a61e
Sebastian <ssalasg.mm@gmail.com>
2024-08-26 18:31:17 -0400

    Agregada otra seccion

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 $(document).ready(function () {
     $("#gato-hero").hover(function () {
+        console.log("Paso el mouse sobre")
         $(this).css({
             "transform": "scale(1.3)",
             'border-bottom': "1px solid white",
             'transition': "all 2s ease",
             "margin-bottom": "3rem"
-        })
-    })
-})
+        });
+    });
+    $("#yo").click(function () {
+        console.log("Clickeado")
+        $("#left").slideDown("slow");
+        $("#rigth").slideDown("slow");
+        $("#img-yo").slideDown("slow");
+    });
+});
